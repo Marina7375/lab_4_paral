a67b0f183ed775a2bcdbd9c1d5076aa2c18729ff
Marina7375 <63257323+Marina7375@users.noreply.github.com>
2021-11-15 10:39:37 +0200

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,60 +8,63 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
-namespace lab_4_32
+
+namespace _4_3
 {
     public partial class Form1 : Form
     {
-        private static Semaphore sem;
-
         public Form1()
         {
             InitializeComponent();
-          
         }
-        
-           
+        class SharedRes
+        {
+            public static Mutex mtx = new Mutex();
+        }
+
         private void TextFirst()
         {
-           
-                // Получить семафор
-                sem.WaitOne();
+            try
+            {
+                // Получить мьютекс
+                SharedRes.mtx.WaitOne();
                 listBox1.Invoke((MethodInvoker)delegate ()
                 {
                     listBox1.Items.Add("First");
                 });
                 Thread.Sleep(50);
-                // Освободить семафор
-                sem.Release();
-                    
+                // Освободить мьютекс
+                SharedRes.mtx.ReleaseMutex();
+            }
+            catch (Exception ex) {  }
         }
         private void TextSecond()
         {
-            
-                // Получить семафор
-                sem.WaitOne();
+            try
+            {
+                // Получить мьютекс
+                SharedRes.mtx.WaitOne();
                 listBox1.Invoke((MethodInvoker)delegate ()
                 {
                     listBox1.Items.Add("Second");
                 });
                 Thread.Sleep(50);
-                // Освободить семафор
-                sem.Release();
-            
+                // Освободить мьютекс
+                SharedRes.mtx.ReleaseMutex();
+            }
+            catch (Exception ex) { }
         }
         private void button1_Click(object sender, EventArgs e)
         {
-           sem= new Semaphore(1, 1);
             for (int i = 0; i < 10; i++)
             {
                 Thread thread1 = new Thread(TextFirst);
                 thread1.Start();
-                 Thread.Sleep(1);
+               // Thread.Sleep(1);
                 Thread thread2 = new Thread(TextSecond);
                 thread2.Start();
             }
-
+   
         }
     }
 }
-
